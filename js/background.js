aabf23583c398d025ea6d798298165fe9f1644ba
Konrad Dzwinel <kdzwinel@gmail.com>
2014-01-21 21:04:03 +0100

    Notifications about smog status change added.

--- js/background.js
+++ js/background.js
@@ -1,20 +1,18 @@
 (function () {
     "use strict";
 
+    function countDangerousPollutants(pollutants) {
+        return pollutants.filter(function(pollutant) {
+            return (pollutant.normPercent > 100);
+        }).length;
+    }
+
     function updateBadge(data) {
-        var danger = 0;
+        var count = countDangerousPollutants(data.pollutants);
 
-        for (var i = 0, l = data.pollutants.length; i < l; i++) {
-            var pollutant = data.pollutants[i];
-
-            if (pollutant.normPercent > 100) {
-                danger++;
-            }
-        }
-
-        chrome.browserAction.setBadgeText({text: '' + danger});
+        chrome.browserAction.setBadgeText({text: '' + count});
         chrome.browserAction.setBadgeBackgroundColor({
-            color: (danger > 0) ? '#FF0000' : '#00FF00'
+            color: (count > 0) ? '#FF0000' : '#00FF00'
         });
     }
 
@@ -31,12 +29,46 @@
         console.warn('Data loading failed.', new Date());
     }
 
+    function checkForStatusChanges(newData) {
+        var deferred = new $.Deferred();
+
+        chrome.storage.local.get(function(oldData) {
+            if(oldData && oldData.pollutants) {
+                var oldValue = countDangerousPollutants(oldData.pollutants),
+                    newValue = countDangerousPollutants(newData.pollutants);
+
+                if(oldValue === 0 && newValue > 0) {
+                    chrome.notifications.create("smog msg", {
+                        type: "basic",
+                        title: "Smog Notification",
+                        message: "Pollution just got really bad, consider staying at home (" + newValue + " pollutant(s) exceeded their norms).",
+                        iconUrl: 'img/icon_64.png'
+                    }, function(){});
+                } else if(newValue === 0 && oldValue > 0) {
+                    chrome.notifications.create("smog msg", {
+                        type: "basic",
+                        title: "Smog Notification",
+                        message: "It's safe to go outside again. There are no pollutants that exceed their norms at the moment.",
+                        iconUrl: 'img/icon_64.png'
+                    }, function(){});
+                }
+            }
+
+            deferred.resolve(newData);
+        });
+
+        return deferred.promise();
+    }
+
     function updateData() {
-        $.getJSON('http://smogalert.pl/api/stats/krakow-krasinskiego')
+        $.when( $.getJSON('http://smogalert.pl/api/stats/krakow-krasinskiego') )
+            .then(checkForStatusChanges)
             .done(saveData)
             .done(updateBadge)
             .fail(loadingFailed);
     }
+
+    window.checkForStatusChanges = checkForStatusChanges;
 
     //this runs only once - when extension is loaded/installed/enabled, everything outside this callback runs every time
     //background page is reloaded
