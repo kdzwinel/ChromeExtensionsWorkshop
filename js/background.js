a9aa1e6a300a9c884fd7d6ec46cbdc1ee28fbd5b
Konrad Dzwinel <kdzwinel@gmail.com>
2014-01-23 15:03:10 +0100

    URL updated: http://www.malopolska.pl/powietrze

--- js/background.js
+++ js/background.js
@@ -68,7 +68,7 @@
 
     function showForecastPage() {
         chrome.tabs.create({
-            url: 'http://www.malopolska.pl/Obywatel/EKO-prognozaMalopolski/Malopolska/Strony/default.aspx',
+            url: 'http://www.malopolska.pl/powietrze',
             active: true
         });
     }
